3b4eed0b80168af17dfa78a295e3dbaf02e29a66
Hineven <hinevenwob@qq.com>
2023-11-13 23:41:34 +0800

    commit missing files.

---
+++ Plugins/MIGI/Source/MIGINN/MIGINN.Build.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using System.IO;
+using UnrealBuildTool;
+
+public class MIGINN : ModuleRules
+{
+    public MIGINN(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+        Type = ModuleType.External;
+
+        PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "include"));
+        // This library is compiled from CMake.
+        PublicAdditionalLibraries.Add(Path.Combine(ModuleDirectory, "MIGINN.lib"));
+    }
+}
